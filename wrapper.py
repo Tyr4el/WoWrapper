5c1d3e7325770b67c302a694284f53d365e30bf1
Nick Conklin <nconkl1@outlook.com>
2017-09-05 23:13:46 -0400

    WIP of get_gear.  Probably won't stick with this giant mess but it's something for now.

--- wrapper.py
+++ wrapper.py
@@ -103,6 +103,41 @@ class WowAPI:
             realm = parsed_json.realm
             average_item_level = parsed_json.items.averageItemLevel
             average_item_level_equipped = parsed_json.items.averageItemLevelEquipped
+            head = {'name': parsed_json.items.head.name, 'icon': parsed_json.items.head.icon, 'quality':
+                parsed_json.items.head.quality}
+            neck = {'name': parsed_json.items.neck.name, 'icon': parsed_json.items.neck.icon, 'quality':
+                parsed_json.items.neck.quality}
+            shoulder = {'name': parsed_json.items.shoulder.name, 'icon': parsed_json.items.shoulder.icon, 'quality':
+                parsed_json.items.shoulder.quality}
+            back = {'name': parsed_json.items.back.name, 'icon': parsed_json.items.back.icon, 'quality':
+                parsed_json.items.back.quality}
+            chest = {'name': parsed_json.items.chest.name, 'icon': parsed_json.items.chest.icon, 'quality':
+                parsed_json.items.chest.quality}
+            wrist = {'name': parsed_json.items.wrist.name, 'icon': parsed_json.items.wrist.icon, 'quality':
+                parsed_json.items.wrist.quality}
+            hands = {'name': parsed_json.items.hands.name, 'icon': parsed_json.items.hands.icon, 'quality':
+                parsed_json.items.hands.quality}
+            waist = {'name': parsed_json.items.waist.name, 'icon': parsed_json.items.waist.icon, 'quality':
+                parsed_json.items.waist.quality}
+            legs = {'name': parsed_json.items.legs.name, 'icon': parsed_json.items.legs.icon, 'quality':
+                parsed_json.items.legs.quality}
+            feet = {'name': parsed_json.items.feet.name, 'icon': parsed_json.items.feet.icon, 'quality':
+                parsed_json.items.feet.quality}
+            finger1 = {'name': parsed_json.items.finger1.name, 'icon': parsed_json.items.finger1.icon, 'quality':
+                parsed_json.items.finger1.quality}
+            finger2 = {'name': parsed_json.items.finger2.name, 'icon': parsed_json.items.finger2.icon, 'quality':
+                parsed_json.items.finger2.quality}
+            trinket1 = {'name': parsed_json.items.trinket1.name, 'icon': parsed_json.items.trinket1.icon, 'quality':
+                parsed_json.items.trinket1.quality}
+            trinket2 = {'name': parsed_json.items.trinket2.name, 'icon': parsed_json.items.trinket2.icon, 'quality':
+                parsed_json.items.trinket2.quality}
+
+            if resp.status == 200:
+                return True, char_name, realm, average_item_level, average_item_level_equipped, head, neck, shoulder,\
+                       back, chest, wrist, hands, waist, legs, feet, finger1, finger2, trinket1, trinket2
+            elif resp.status == 404:
+                reason = parsed_json.reason
+                return False, reason
 
     async def get_mounts(self, realm, character):
         async with self.session.get(self.base_url +
