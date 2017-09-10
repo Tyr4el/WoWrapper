fcb8f60098f6e3f09af56c36e511b02dff7acd74
Nick Conklin <nconkl1@outlook.com>
2017-09-10 00:17:27 -0400

    Add item.py (broken)

---
+++ item.py
@@ -0,0 +1,18 @@
+import json
+
+
+class Items:
+    def __init__(self):
+        with open('ItemSparse.json') as data_file:
+            self.parsed_json = json.load(data_file)
+
+    def get_item(self, item_name):
+        for item in self.parsed_json:
+            if item['Name'] == item_name:
+                return True, {'name': item_name, 'item level': item['ItemLevel'], 'quality': item['Quality'],
+                              'id': item['m_ID']}
+            else:
+                return False
+
+i = Items()
+print(i.get_item('Worn Axe'))
